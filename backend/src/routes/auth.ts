abdd460636f497f1d8d0a56bdee722b9edd99a95
Suraj Sharma <suraj200delhi@gmail.com>
2025-01-02 11:24:28 +0530

    Add zod validation to signin endpoint

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -14,11 +14,11 @@ if (!JWT_SECRET) {
 }
 
 // Signup Route
-router.post("/signup", async (req: Request, res:Response) => {
+router.post("/signup", async (req: Request, res: Response) => {
   // Zod Validation
   const requiredBody = z.object({
     name: z.string().min(6).max(30),
-    email: z.string().min(6).max(100).email(),
+    email: z.string().min(6).max(320).email(),
     password: z.string().min(8).max(30),
     phone_no: z.string().min(10).max(10),
     address: z.array(
@@ -34,12 +34,12 @@ router.post("/signup", async (req: Request, res:Response) => {
 
   const parsedData = requiredBody.safeParse(req.body);
   if (!parsedData.success) {
-     res.status(400).json({
+    res.status(400).json({
       message: "Incorrect signup Format  ",
     });
     return
   }
-  const { name, email, password, phone_no, address } = parsedData.data ;
+  const { name, email, password, phone_no, address } = parsedData.data;
 
   try {
     const emailExists = await UsersModel.findOne({ email });
@@ -48,7 +48,7 @@ router.post("/signup", async (req: Request, res:Response) => {
     // console.log(emailExists ? emailExists.email : "email not exit");
     // console.log(phone_noExists ? phone_noExists.phone_no : "phone_no not exit");
     if (phone_noExists || emailExists) {
-       res.status(409).json({
+      res.status(409).json({
         message: "User Already Exists",
       });
       return
@@ -67,7 +67,7 @@ router.post("/signup", async (req: Request, res:Response) => {
     });
   } catch (error) {
     console.log(error);
-     res.status(500).json({
+    res.status(500).json({
       message: "Server error occurred",
     });
     return
@@ -78,16 +78,24 @@ router.post("/signup", async (req: Request, res:Response) => {
 // Signin Route
 
 router.post("/signin", async (req: Request, res: Response) => {
-  const { email, password }  = req.body;
-// Use zod
+  const requiredSignin = z.object({
+    email: z.string().max(320).email(),
+    password: z.string().min(8).max(30)
 
+  })
+  const parsedSignin = requiredSignin.safeParse(req.body)
 
-
-
+  if (!parsedSignin.success) {
+    res.status(400).json({
+      message: "Incorrect Signin format"
+    })
+    return
+  }
   try {
+    const { email, password } = parsedSignin.data
     const user = await UsersModel.findOne({ email });
     if (!user) {
-       res.status(400).send({
+      res.status(400).send({
         message: "Email Not Found",
       });
       return
@@ -95,7 +103,7 @@ router.post("/signin", async (req: Request, res: Response) => {
 
     const passwordCheck = await bcrypt.compare(password, user.password);
     if (!passwordCheck) {
-       res.status(403).send({
+      res.status(403).send({
         message: "Invalid name or Password",
       });
       return
@@ -115,7 +123,7 @@ router.post("/signin", async (req: Request, res: Response) => {
       token: token,
     });
   } catch (error) {
-     res.status(500).send({
+    res.status(500).send({
       message: `Error occurred ${error}`,
     });
     return
